6ce29a6dc18bed5c3985c9626555a5ba47c476bc
makhosi6 <ndondo330@gmail.com>
2021-08-16 13:47:21 +0200

    Mon Aug 16 2021 13:45:15 - Nav Links

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="app">
-    <!-- <div id="nav">
+  <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> | 
       <router-link to="/messages">Messages</router-link>
-    </div> -->
+    </div> 
     <mobile-wrapper v-if="hasLoaded" >
       <menu-top></menu-top>
       <router-view />
